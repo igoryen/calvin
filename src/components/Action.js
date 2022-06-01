ad2059e52a0740c5cdc4b57eb5f235470ba724b0
igoryen <igor.yentaltsev@gmail.com>
2022-06-01 16:23:27 -0400

    app works

---
+++ src/components/Action.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Action = (props) => (
+    <div>
+        <button
+            className="big-button"
+            onClick={props.handlePick}
+            disabled={!props.hasOpts}
+        >
+            What should I do?
+        </button>
+    </div>
+);
+export default Action;
